ba25b16b108a6cc314964878bbf6b2199a79747c
Tom Kooij <tomkooij@tomkooij.nl>
2019-11-06 08:30:53 +0100

    Build HDF5 lib threadsafe

--- config.sh
+++ config.sh
@@ -14,7 +14,23 @@ function build_wheel {
 function build_libs {
     build_bzip2
     build_lzo
-    build_hdf5
+    build_hdf5_threadsafe
+}
+
+function build_hdf5_threadsafe {
+    if [ -e hdf5-stamp ]; then return; fi
+    build_zlib
+    # libaec is a drop-in replacement for szip
+    build_libaec
+    local hdf5_url=https://support.hdfgroup.org/ftp/HDF5/releases
+    local short=$(echo $HDF5_VERSION | awk -F "." '{printf "%d.%d", $1, $2}')
+    fetch_unpack $hdf5_url/hdf5-$short/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
+    (cd hdf5-$HDF5_VERSION \
+        && ./configure --with-szlib=$BUILD_PREFIX --prefix=$BUILD_PREFIX \
+        --enable-threadsafe --enable-unsupported --with-pthread=yes \
+        && make -j4 \
+        && make install)
+    touch hdf5-stamp
 }
 
 function build_linux_wheel {
